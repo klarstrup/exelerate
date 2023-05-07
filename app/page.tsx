e062aecfb3a01ef508f587a7a051fbd03f7017ad
klarstrup <io@klarstrup.dk>
2023-05-07 17:33:42 +0200

    port exelerate.dk from PHP to next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,205 @@
-import Image from 'next/image'
+import { formatDistanceToNow, setHours } from "date-fns";
+import { Fragment, use } from "react";
+
+export namespace Songkick {
+  export interface Event {
+    id: number;
+    displayName: string;
+    type: Type;
+    uri: string;
+    status: Status;
+    popularity: number;
+    start: End;
+    performance: Performance[];
+    ageRestriction: null | string;
+    flaggedAsEnded?: boolean;
+    venue: Venue;
+    location: Location;
+    end?: End;
+    series?: Series;
+  }
+
+  export interface End {
+    date: Date;
+    time: null | string;
+    datetime: null | string;
+  }
+
+  export interface Location {
+    city: string;
+    lat: number;
+    lng: number;
+  }
+
+  export interface Performance {
+    id: number;
+    displayName: string;
+    billing: Billing;
+    billingIndex: number;
+    artist: MetroArea;
+  }
+
+  export interface MetroArea {
+    id: number;
+    displayName: string;
+    uri: string;
+    identifier?: Identifier[];
+    country?: Series;
+  }
+
+  export interface Series {
+    displayName: string;
+  }
+
+  export interface Identifier {
+    mbid: string;
+    href: string;
+  }
+
+  export enum Billing {
+    Headline = "headline",
+    Support = "support",
+  }
+
+  export enum Status {
+    Ok = "ok",
+  }
+
+  export enum Type {
+    Concert = "Concert",
+    Festival = "Festival",
+  }
+
+  export interface Venue {
+    id: number | null;
+    displayName: string;
+    uri: null | string;
+    metroArea: MetroArea;
+    lat: number | null;
+    lng: number | null;
+  }
+}
 
 export default function Home() {
+  const nextShow: Songkick.Event = use(
+    fetch(
+      `https://api.songkick.com/api/3.0/artists/6777179-exelerate/calendar.json?apikey=${process.env.SONGKICK_APIKEY}`,
+      { next: { revalidate: 3600 } }
+    )
+      .then((res) => res.json())
+      .then((res) => res.resultsPage?.results?.event?.[0])
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <>
+      <img
+        src="img/fullLogo-min.png"
+        alt="Exelelogo"
+        style={{ flex: 0, marginTop: 16, height: "40vh", width: "auto" }}
+      />
+      <div
+        style={{
+          textTransform: "uppercase",
+          justifyContent: "space-evenly",
+          position: "relative",
+          display: "flex",
+          fontSize: "3em",
+          flexDirection: "row",
+          textShadow: "0 0 12px rgba(0,0,0,1)",
+          flexWrap: "wrap",
+          alignItems: "flex-start",
+        }}
+      >
+        <div
+          id="exeleLinks"
+          style={{
+            marginTop: "2.5%",
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            marginBottom: "2.5%",
+          }}
+        >
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.facebook.com/Exelerateband"
+            title="Main social"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Facebook
+          </a>
+          <a
+            href="https://www.instagram.com/exeleratedk"
+            title="Secondary social"
+            target="_blank"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://open.spotify.com/artist/0jRw54h5J5HeIkexDiOHZC"
+            title="song downloading/purchasing"
+            target="_blank"
+          >
+            Spotify
+          </a>
+          <a
+            href="https://youtube.com/user/ExelerateDK"
+            title="Videos"
+            target="_blank"
+          >
+            YouTube
           </a>
         </div>
+        {nextShow ? (
+          <div className="nextGig">
+            Next show is
+            <div>
+              in{" "}
+              <a target="_blank" href={nextShow.uri}>
+                <time
+                  title={`${nextShow.start.date}  ${nextShow.start.time}`}
+                  dateTime={nextShow.start.datetime || undefined}
+                >
+                  {formatDistanceToNow(
+                    new Date(
+                      nextShow.start.datetime ||
+                        setHours(new Date(nextShow.start.date), 16) ||
+                        ""
+                    )
+                  )}
+                </time>
+              </a>{" "}
+              at
+            </div>
+            <a
+              className="venue"
+              target="_blank"
+              href={`https://maps.google.com/maps?q=${nextShow.venue.displayName}`}
+            >
+              <span style={{ whiteSpace: "nowrap" }}>
+                {nextShow.venue.displayName}
+              </span>
+            </a>
+            {nextShow.performance.length > 1 ? (
+              <>
+                <br />
+                with{" "}
+                <div className="otherHeadliners">
+                  {nextShow.performance
+                    .filter((p) => p.displayName !== "Exelerate")
+                    .map((p, i) => (
+                      <Fragment key={p.artist.id}>
+                        {i ? <> </> : null}
+                        <a target="_blank" href={p.artist.uri}>
+                          <em>{p.displayName}</em>
+                        </a>
+                      </Fragment>
+                    ))}
+                </div>
+              </>
+            ) : null}
+          </div>
+        ) : null}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  rewrites: () => [{ source: "/tech", destination: "/tech/index.html" }],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
