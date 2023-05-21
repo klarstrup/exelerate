365118d87fded9427e9ad1c166d16cf59e1400e7
klarstrup <io@klarstrup.dk>
2023-05-21 17:06:12 +0200

    use image imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { formatDistanceToNow, setHours } from "date-fns";
+import Image from "next/image";
 import { Fragment } from "react";
 import FTVLogo from "./ftv-logo.png";
-import Image from "next/image";
+import logoImg from "./logo.png";
 
 export const revalidate = 60;
 
@@ -94,136 +95,151 @@ export default async function Home() {
 
   return (
     <>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="img/fullLogo-min.png"
+        src={logoImg.src}
         alt="Exelelogo"
-        style={{ flex: 0, marginTop: 16, height: "40vh", width: "auto" }}
+        style={{
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+          height: "40vh",
+          width: `${0.4 * (logoImg.width / logoImg.height) * 100}vh`,
+        }}
       />
       <div
         style={{
-          textTransform: "uppercase",
-          justifyContent: "space-evenly",
-          position: "relative",
           display: "flex",
-          fontSize: "3em",
-          flexDirection: "row",
-          textShadow: "0 0 12px rgba(0,0,0,1)",
-          flexWrap: "wrap",
-          alignItems: "flex-start",
-          paddingTop: "5vh",
-          paddingBottom: "10vh",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
         <div
-          id="exeleLinks"
           style={{
+            textTransform: "uppercase",
+            justifyContent: "space-evenly",
+            position: "relative",
             display: "flex",
-            flexDirection: "column",
-            textAlign: "center",
-            marginBottom: "2.5%",
+            fontSize: "3em",
+            flexDirection: "row",
+            textShadow: "0 0 12px rgba(0,0,0,1)",
+            flexWrap: "wrap",
+            alignItems: "flex-start",
+            paddingTop: "5vh",
+            paddingBottom: "10vh",
           }}
         >
-          <a
-            href="https://www.facebook.com/Exelerateband"
-            title="Main social"
-            target="_blank"
+          <div
+            id="exeleLinks"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              marginBottom: "2.5%",
+            }}
           >
-            Facebook
-          </a>
-          <a
-            href="https://www.instagram.com/exeleratedk"
-            title="Secondary social"
-            target="_blank"
-          >
-            Instagram
-          </a>
-          <a
-            href="https://open.spotify.com/artist/0jRw54h5J5HeIkexDiOHZC"
-            title="song downloading/purchasing"
-            target="_blank"
-          >
-            Spotify
-          </a>
-          <a
-            href="https://youtube.com/user/ExelerateDK"
-            title="Videos"
-            target="_blank"
-          >
-            YouTube
-          </a>
-          <a
-            href="https://fromthevaults.dk/"
-            target="_blank"
-            title="From The Vaults"
-            className="ftvLink"
-            style={{ position: "fixed", top: "5vh", right: "5vw" }}
-          >
-            <Image
-              src={FTVLogo}
-              alt="From The Vaults"
-              style={{
-                maxWidth: "7.5vh",
-                height: "auto",
-                marginTop: "0.125em",
-              }}
-            />
-          </a>
-        </div>
-        {nextShow ? (
-          <div className="nextGig">
-            Next show is
-            <div>
-              in{" "}
-              <a target="_blank" href={nextShow.uri}>
-                <time
-                  title={`${nextShow.start.date}  ${nextShow.start.time}`}
-                  dateTime={nextShow.start.datetime || undefined}
-                >
-                  {formatDistanceToNow(
-                    new Date(
-                      nextShow.start.datetime ||
-                        setHours(new Date(nextShow.start.date), 16) ||
-                        ""
-                    )
-                  )}
-                </time>
-              </a>{" "}
-              at
-            </div>
             <a
-              className="venue"
+              href="https://www.facebook.com/Exelerateband"
+              title="Main social"
               target="_blank"
-              href={`https://maps.google.com/maps?q=${
-                (nextShow.venue.id && nextShow.venue.displayName) ||
-                nextShow.location.city
-              }`}
             >
-              <span style={{ whiteSpace: "nowrap" }}>
-                {nextShow.series?.displayName ||
-                  (nextShow.venue.id && nextShow.venue.displayName) ||
-                  nextShow.location.city}
-              </span>
+              Facebook
             </a>
-            {nextShow.performance.length > 1 ? (
-              <>
-                <br />
-                with{" "}
-                <div className="otherHeadliners">
-                  {nextShow.performance
-                    .filter((p) => p.displayName !== "Exelerate")
-                    .map((p, i) => (
-                      <Fragment key={p.artist.id}>
-                        {i ? <> </> : null}
-                        <a target="_blank" href={p.artist.uri}>
-                          <em>{p.displayName}</em>
-                        </a>
-                      </Fragment>
-                    ))}
-                </div>
-              </>
-            ) : null}
+            <a
+              href="https://www.instagram.com/exeleratedk"
+              title="Secondary social"
+              target="_blank"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://open.spotify.com/artist/0jRw54h5J5HeIkexDiOHZC"
+              title="song downloading/purchasing"
+              target="_blank"
+            >
+              Spotify
+            </a>
+            <a
+              href="https://youtube.com/user/ExelerateDK"
+              title="Videos"
+              target="_blank"
+            >
+              YouTube
+            </a>
+            <a
+              href="https://fromthevaults.dk/"
+              target="_blank"
+              title="From The Vaults"
+              className="ftvLink"
+              style={{ position: "fixed", top: "5vh", right: "5vw" }}
+            >
+              <Image
+                src={FTVLogo}
+                alt="From The Vaults"
+                style={{
+                  maxWidth: "7.5vh",
+                  height: "auto",
+                  marginTop: "0.125em",
+                }}
+              />
+            </a>
           </div>
-        ) : null}
+          {nextShow ? (
+            <div className="nextGig">
+              Next show is
+              <div>
+                in{" "}
+                <a target="_blank" href={nextShow.uri}>
+                  <time
+                    title={`${nextShow.start.date}  ${nextShow.start.time}`}
+                    dateTime={nextShow.start.datetime || undefined}
+                  >
+                    {formatDistanceToNow(
+                      new Date(
+                        nextShow.start.datetime ||
+                          setHours(new Date(nextShow.start.date), 16) ||
+                          ""
+                      )
+                    )}
+                  </time>
+                </a>{" "}
+                at
+              </div>
+              <a
+                className="venue"
+                target="_blank"
+                href={`https://maps.google.com/maps?q=${
+                  (nextShow.venue.id && nextShow.venue.displayName) ||
+                  nextShow.location.city
+                }`}
+              >
+                <span style={{ whiteSpace: "nowrap" }}>
+                  {nextShow.series?.displayName ||
+                    (nextShow.venue.id && nextShow.venue.displayName) ||
+                    nextShow.location.city}
+                </span>
+              </a>
+              {nextShow.performance.length > 1 ? (
+                <>
+                  <br />
+                  with{" "}
+                  <div className="otherHeadliners">
+                    {nextShow.performance
+                      .filter((p) => p.displayName !== "Exelerate")
+                      .map((p, i) => (
+                        <Fragment key={p.artist.id}>
+                          {i ? <> </> : null}
+                          <a target="_blank" href={p.artist.uri}>
+                            <em>{p.displayName}</em>
+                          </a>
+                        </Fragment>
+                      ))}
+                  </div>
+                </>
+              ) : null}
+            </div>
+          ) : null}
+        </div>
       </div>
     </>
   );
