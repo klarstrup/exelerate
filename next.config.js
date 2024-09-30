1499b041d41057ba634ddef691d9efcf2d8c3d98
Io Klarstrup <io@klarstrup.dk>
2024-09-30 23:24:32 +0200

    add no-iem rider

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: () => [{ source: "/tech", destination: "/tech/index.html" }],
+  rewrites: () => [
+    { source: "/tech", destination: "/tech/index.html" },
+    { source: "/tech/iem", destination: "/tech/index.html" },
+    { source: "/tech/no-iem", destination: "/tech/no-iem.html" },
+  ],
 };
 
 module.exports = nextConfig;
