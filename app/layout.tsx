fc6a4f88852d57c4c61a67e495454c86b817fbe3
klarstrup <io@klarstrup.dk>
2025-06-06 13:08:57 +0200

    feat: add Twitter tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,14 @@ export default function RootLayout({
         <meta name="author" content="Stefan Jensen" />
         <meta name="author" content="Stig Eilsøe-Madsen" />
         <meta name="author" content="Mads Nikolai Sørensen" />
+        <script>
+          {`
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','ptxbg');
+          `}
+        </script>
       </head>
       <body
         className={bebasNeue.className}
